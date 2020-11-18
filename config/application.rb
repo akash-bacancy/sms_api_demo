65f2923a34fcabd12c9fd226e49024957e44c9d0
akash-bacancy <akash.pande@bacancytechnology.com>
2020-11-18 17:02:24 +0530

    changes in application.rb

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,6 @@ module ApiDemo
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
     config.autoload_paths << "#{Rails.root}/lib"
-    config.eager_load_paths += Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
   end
 end
