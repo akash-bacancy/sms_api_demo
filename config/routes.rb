314764bb9f2672b006879f04763937b8f8996c1a
akash-bacancy <akash.pande@bacancytechnology.com>
2020-11-18 16:39:49 +0530

    init other configs

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+  	post '/inbound/sms/', to: 'sms_sender#inbound_sms'
+  	post '/outbound/sms/', to: 'sms_sender#outbound_sms'
+  end
+end
